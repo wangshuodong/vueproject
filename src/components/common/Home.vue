10b3a75ab7a6ca2894ed92503f476b060e324d7e
wangshuodong <wangshuodong2004@163.com>
2018-11-08 17:54:54 +0800

    Initial commit

---
+++ src/components/common/Home.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="wrapper">
+        <Header></Header>
+        <Sidebar></Sidebar>
+        111111
+    </div>
+</template>
+
+<script>
+    import Header from './Header.vue';
+    import Sidebar from './Sidebar.vue';
+    export default {
+        data() {
+            return {
+                tagsList: [],
+                collapse: false
+            }
+        },
+        components: {
+            Header,
+            Sidebar
+        }
+    }
+</script>
+
+<style>
+</style>
